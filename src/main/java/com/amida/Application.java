ba8a170e13695725e97080d349cbb08b17ae5a4d
m-ox <maudlin.oxalis@gmail.com>
2022-11-29 19:07:20 -0700

    postgresql account management api, currently not generating IDs in postgres for some reason

--- src/main/java/com/amida/Application.java
+++ src/main/java/com/amida/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2Application {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(H2Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
--- src/main/java/com/amida/domain/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.amida.domain.repository;
-
-import com.amida.domain.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
